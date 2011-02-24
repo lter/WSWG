092b40e5e0d726a92b6fcec9e71a36535fa813ee
sbohm <sbohm@7e66d0b5-1a0d-43fa-8b78-4cc5a103f8f0>
2011-02-24 18:41:28 +0000

    basic xml serialzation for person

--- personnelDB/lib/entities/Person.php
+++ personnelDB/lib/entities/Person.php
@@ -23,4 +23,17 @@ class Person extends Entity {
 
   public function getContactInfo() { }
 
+  // returns a representation of itself as an xml string that conforms to the personelDB.xsd 
+  public to_xml() {
+    $this->xml_obj = new DOMElement('person');
+    $this->person_id = new DOMElement('personID');
+    $this->xml_obj.appendChild($this->xml_obj->createElement('personID',getIdentity()->personID));
+    $this->xml_obj.appendChild(getIdentity()->to_xml());
+    if (getRoles()->length > 0) {
+      $this->xml_obj.appendChild(getRoles()->to_xml());
+    }
+    if (getContactInfo()->length > 0) {
+      $this->xml_obj.appendChild(getContactInfo());
+    }
+  }
 }
