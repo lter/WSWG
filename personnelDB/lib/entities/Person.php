6b708e016598e691fed0bd04d0bf21d9ca6f5caa
sbohm <sbohm@7e66d0b5-1a0d-43fa-8b78-4cc5a103f8f0>
2011-02-25 16:36:36 +0000

    finished deserialization on person NOT tested

--- personnelDB/lib/entities/Person.php
+++ personnelDB/lib/entities/Person.php
@@ -51,9 +51,30 @@ class Person extends Entity {
     $xpath = new \DOMXPath($xml_dom);
     $this->personID = $xpath.query("*/personID/")->nodeValue;
     $roles = array();
-    foreach($xpath.query("*/roleList/role") as $role_element) {
-      //TODO: this is not right
-      $roles.push($role_element->nodeName);
+
+    foreach($xpath->query("*/roleList/role") as $role_element) {
+      $role_xpath = new \DOMXPath($role_element);
+      $node_id = $role_xpath.query("/roleID")->nodeValue;
+      if ($node_id) {
+        $role = $this->storeFront->RoleStore->getById($node_id);
+      } else {
+        $role = new Role();
+      }
+      $role->from_xml($role_element);
+      $roles->push($role);
+    }
+    unset($role_element);
+
+    foreach($xpath->query("*/contactInfoList/contact") as $contact_element) {
+      $contact_xpath = new \DOMXPath($contact_element);
+      $contact_id = $contact_xpath.query("/contactInfoID")->nodeValue;
+      if ($contact_id){
+        $contact = $this->storeFront->ContactInfoStore->getById($contact_id);
+      } else {
+        $contact = new Contact();
+      }
+      $contact->from_xml($contact_element);
+      $contacts->pusth($contact);
     }
   }
 }
